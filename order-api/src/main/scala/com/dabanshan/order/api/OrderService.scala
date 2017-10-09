42c5c2ec92c11a6166262a4c20338db9e268dabf
laidingqing <laidingqing@gmail.com>
2017-10-09 14:22:04 +0800

    add order service

--- order-api/src/main/scala/com/dabanshan/order/api/OrderService.scala
+++ order-api/src/main/scala/com/dabanshan/order/api/OrderService.scala
@@ -1,7 +1,8 @@
 package com.dabanshan.order.api
 
-import com.dabanshan.order.api.model.request.OrderCreation
-import com.dabanshan.order.api.model.response.CreationOrderDone
+import akka.{Done, NotUsed}
+import com.dabanshan.order.api.model.request.{GetOrder, OrderCreation}
+import com.dabanshan.order.api.model.response.{CreationOrderDone, GetOrderDone}
 import com.lightbend.lagom.scaladsl.api.transport.Method
 import com.lightbend.lagom.scaladsl.api.{CircuitBreaker, Service, ServiceCall}
 
@@ -16,12 +17,28 @@ trait OrderService extends Service {
     */
   def createOrder(): ServiceCall[OrderCreation, CreationOrderDone]
 
+  /**
+    * 获取订单
+    * @param id
+    * @return
+    */
+  def getOrder(id: String): ServiceCall[GetOrder, GetOrderDone]
+
+  /**
+    * 更新订单(状态)
+    * @param id
+    * @return
+    */
+  def updateOrder(id: String): ServiceCall[NotUsed, Done]
+
   override final def descriptor = {
     import Service._
     // @formatter:off
     named("orders")
       .withCalls(
-        restCall(Method.POST, "/api/orders/", createOrder )
+        restCall(Method.POST, "/api/orders/", createOrder ),
+        restCall(Method.POST, "/api/orders/:id", getOrder _),
+        restCall(Method.PUT, "/api/orders/:id", updateOrder _)
       )
       .withAutoAcl(true)
       .withCircuitBreaker(CircuitBreaker.PerNode)
---
+++ order-api/src/main/scala/com/dabanshan/order/api/model/response/GetOrderDone.scala
@@ -0,0 +1,23 @@
+package com.dabanshan.order.api.model.response
+
+import java.util.Date
+
+import com.dabanshan.commons.response.GeneratedIdDone
+import play.api.libs.json.{Format, Json}
+
+/**
+  * Created by skylai on 2017/10/5.
+  */
+case class GetOrderItem(id: String, name: String, price: BigDecimal)
+
+case class GetOrderDone(
+  id: String,
+  createdAt: Date,
+  amount: BigDecimal,
+  status: Integer,
+  items: Seq[GetOrderItem]
+) extends GeneratedIdDone
+
+object GetOrderDone {
+  implicit val format: Format[GetOrderDone] = Json.format
+}
